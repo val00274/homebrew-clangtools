9b91cb750c8f40b501428b0a9ac76423beb6d29e
asatou <asatou@val.co.jp>
2016-10-31 11:40:49 +0900

    delete sha1 digest in `clang-tools.rb`

--- clang-tools.rb
+++ clang-tools.rb
@@ -4,7 +4,6 @@ CLANG_TOOLS_VERSION = '3.4.20140401'
 class ClangTools < Formula
   homepage 'http://llvm.org/'
   url 'https://github.com/val00274/clang-tools.git', :tag => "v#{CLANG_TOOLS_VERSION}"
-  sha1 '1b43cc7827b9fa4e017614f2eb17bc35998d461c'
   head 'https://github.com/val00274/clang-tools.git', :branch => 'master'
   version CLANG_TOOLS_VERSION
 
